7bb47bb688c20d1ebf223b96b7d8175b2b584973
Pivcevic <pivcevic@685b35a27f66.ant.amazon.com>
2014-05-03 10:35:28 +0100

    hostname

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
+import os
+hostname = os.environ.get('HOSTNAME','?')
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World from %s!" % (hostname,)
 
 if __name__ == "__main__":
     app.run(port=8080)
