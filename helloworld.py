b2d4ec71b8fb5c1546551e618c5874fbd8a158e3
Pivcevic <pivcevic@685b35a27f66.ant.amazon.com>
2014-05-05 14:10:08 +0100

    socket instead od env hostname

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 import os
+import socket
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from %s!" % (os.environ.get('HOSTNAME','?'),)
+    return "Hello World from %s!" % (socket.gethostname(),)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080)
