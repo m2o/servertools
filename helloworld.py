8e95d97cbb4a2823f2c4ad72b32155138d28faba
Pivcevic <pivcevic@685b35a27f66.ant.amazon.com>
2014-05-05 13:14:03 +0100

    hostname at each invocation

--- helloworld.py
+++ helloworld.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 import os
-hostname = os.environ.get('HOSTNAME','?')
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from %s!" % (hostname,)
+    return "Hello World from %s!" % (os.environ.get('HOSTNAME','?'),)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080)
